11f6a9eda52a8c96a7865e6d09f78b3a64bdfaa8
Lucas Côrte <lucascorte@outlook.com>
2022-02-14 13:48:17 -0300

    Seller entity class

--- src/appication/Program.java
+++ src/appication/Program.java
@@ -1,10 +1,19 @@
 package appication;
 
+import java.util.Date;
+
+import model.entites.Department;
+import model.entites.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Department department = new Department(1, "Books");
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 2500.0, department);
 		
+		System.out.println(department);
+		System.out.println(seller);
 	}
 
 }
